50c9ac35ca0044fc5f2f6f186e81bfbadbaeaeef
Maxwell Lokshin <139578323+maxwelllokshin1@users.noreply.github.com>
2026-06-29 01:00:45 -0400

    Reorder CORS middleware initialization

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const cors = require("cors");
-app.use(cors());
-
 require("dotenv").config(); // require env file
 console.log("URI loaded:", !!process.env.MONGO_URI); // should print true
 const express = require("express"); // server that seperately talks to db
 const connectDB = require("./db"); 
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json()); // start server
 connectDB(); // connect to the db
 
